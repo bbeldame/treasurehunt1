1a1b957935f4a580df9ab2a6c3fb7ec3f21446dc
Basile Beldame <basile.beldame@supdeweb-paris.com>
2020-03-30 04:31:09 +0200

    cool

---
+++ pages/6EOWV3.js
@@ -0,0 +1,444 @@
+import { setCookie } from "nookies";
+
+export default function Index() {
+  return (
+    <>
+      <header
+        style={{
+          backgroundImage:
+            "url('https://images.unsplash.com/photo-1485745655111-3272a37e76a5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80')"
+        }}
+      >
+        <nav></nav>
+        <h1>
+          <marquee>
+            🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪Look me🍪🍪🍪🍪🍪🍪Hello🍪🍪🍪🍪🍪🍪
+            Look at me !
+          </marquee>
+        </h1>
+        <h1>
+          <marquee>
+            🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪Look me🍪🍪🍪🍪🍪🍪Hello🍪🍪🍪🍪🍪🍪
+            Look at me !
+          </marquee>
+        </h1>
+        <h1>
+          <marquee>
+            🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪Look me🍪🍪🍪🍪🍪🍪Hello🍪🍪🍪🍪🍪🍪
+            Look at me !
+          </marquee>
+        </h1>
+        <h1>
+          <marquee>
+            🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪Look me🍪🍪🍪🍪🍪🍪Hello🍪🍪🍪🍪🍪🍪
+            Look at me !
+          </marquee>
+        </h1>
+        <h1>
+          <marquee>
+            🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪Look me🍪🍪🍪🍪🍪🍪Hello🍪🍪🍪🍪🍪🍪
+            Look at me !
+          </marquee>
+        </h1>
+        <h1>
+          <marquee>
+            🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪Look me🍪🍪🍪🍪🍪🍪Hello🍪🍪🍪🍪🍪🍪
+            Look at me !
+          </marquee>
+        </h1>
+        <h1>
+          <marquee>
+            🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪Look me🍪🍪🍪🍪🍪🍪Hello🍪🍪🍪🍪🍪🍪
+            Look at me !
+          </marquee>
+        </h1>
+        <h1>
+          <marquee>
+            🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪Look me🍪🍪🍪🍪🍪🍪Hello🍪🍪🍪🍪🍪🍪
+            Look at me !
+          </marquee>
+        </h1>
+        <h1>
+          <marquee>
+            🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪Look me🍪🍪🍪🍪🍪🍪Hello🍪🍪🍪🍪🍪🍪
+            Look at me !
+          </marquee>
+        </h1>
+        <h1>
+          <marquee>
+            🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪Look me🍪🍪🍪🍪🍪🍪Hello🍪🍪🍪🍪🍪🍪
+            Look at me !
+          </marquee>
+        </h1>
+        <h1>
+          <marquee>
+            🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪Look me🍪🍪🍪🍪🍪🍪Hello🍪🍪🍪🍪🍪🍪
+            Look at me !
+          </marquee>
+        </h1>
+        <h1>
+          <marquee>
+            🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪Look me🍪🍪🍪🍪🍪🍪Hello🍪🍪🍪🍪🍪🍪
+            Look at me !
+          </marquee>
+        </h1>
+        <h1>
+          <marquee>
+            🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪Look me🍪🍪🍪🍪🍪🍪Hello🍪🍪🍪🍪🍪🍪
+            Look at me !
+          </marquee>
+        </h1>
+        <h1>
+          <marquee>
+            🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪Look me🍪🍪🍪🍪🍪🍪Hello🍪🍪🍪🍪🍪🍪
+            Look at me !
+          </marquee>
+        </h1>
+        <h1>
+          <marquee>
+            🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪Look me🍪🍪🍪🍪🍪🍪Hello🍪🍪🍪🍪🍪🍪
+            Look at me !
+          </marquee>
+        </h1>
+        <h1>
+          <marquee>
+            🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪Look me🍪🍪🍪🍪🍪🍪Hello🍪🍪🍪🍪🍪🍪
+            Look at me !
+          </marquee>
+        </h1>
+        <h1>
+          <marquee>
+            🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪Look me🍪🍪🍪🍪🍪🍪Hello🍪🍪🍪🍪🍪🍪
+            Look at me !
+          </marquee>
+        </h1>
+        <h1>
+          <marquee>
+            🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪Look me🍪🍪🍪🍪🍪🍪Hello🍪🍪🍪🍪🍪🍪
+            Look at me !
+          </marquee>
+        </h1>
+        <h1>
+          <marquee>
+            🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪Look me🍪🍪🍪🍪🍪🍪Hello🍪🍪🍪🍪🍪🍪
+            Look at me !
+          </marquee>
+        </h1>
+        <h1>
+          <marquee>
+            🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪Look me🍪🍪🍪🍪🍪🍪Hello🍪🍪🍪🍪🍪🍪
+            Look at me !
+          </marquee>
+        </h1>
+        <h1>
+          <marquee>
+            🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪Look me🍪🍪🍪🍪🍪🍪Hello🍪🍪🍪🍪🍪🍪
+            Look at me !
+          </marquee>
+        </h1>
+        <h1>
+          <marquee>
+            🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪Look me🍪🍪🍪🍪🍪🍪Hello🍪🍪🍪🍪🍪🍪
+            Look at me !
+          </marquee>
+        </h1>
+        <h1>
+          <marquee>
+            🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪Look me🍪🍪🍪🍪🍪🍪Hello🍪🍪🍪🍪🍪🍪
+            Look at me !
+          </marquee>
+        </h1>
+        <h1>
+          <marquee>
+            🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪Look me🍪🍪🍪🍪🍪🍪Hello🍪🍪🍪🍪🍪🍪
+            Look at me !
+          </marquee>
+        </h1>
+        <h1>
+          <marquee>
+            🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪Look me🍪🍪🍪🍪🍪🍪Hello🍪🍪🍪🍪🍪🍪
+            Look at me !
+          </marquee>
+        </h1>
+        <h1>
+          <marquee>
+            🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪Look me🍪🍪🍪🍪🍪🍪Hello🍪🍪🍪🍪🍪🍪
+            Look at me !
+          </marquee>
+        </h1>
+        <h1>
+          <marquee>
+            🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪Look me🍪🍪🍪🍪🍪🍪Hello🍪🍪🍪🍪🍪🍪
+            Look at me !
+          </marquee>
+        </h1>
+        <h1>
+          <marquee>
+            🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪Look me🍪🍪🍪🍪🍪🍪Hello🍪🍪🍪🍪🍪🍪
+            Look at me !
+          </marquee>
+        </h1>
+        <h1>
+          <marquee>
+            🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪Look me🍪🍪🍪🍪🍪🍪Hello🍪🍪🍪🍪🍪🍪
+            Look at me !
+          </marquee>
+        </h1>
+        <h1>
+          <marquee>
+            🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪Look me🍪🍪🍪🍪🍪🍪Hello🍪🍪🍪🍪🍪🍪
+            Look at me !
+          </marquee>
+        </h1>
+        <h1>
+          <marquee>
+            🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪Look me🍪🍪🍪🍪🍪🍪Hello🍪🍪🍪🍪🍪🍪
+            Look at me !
+          </marquee>
+        </h1>
+        <h1>
+          <marquee>
+            🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪Look me🍪🍪🍪🍪🍪🍪Hello🍪🍪🍪🍪🍪🍪
+            Look at me !
+          </marquee>
+        </h1>
+        <h1>
+          <marquee>
+            🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪Look me🍪🍪🍪🍪🍪🍪Hello🍪🍪🍪🍪🍪🍪
+            Look at me !
+          </marquee>
+        </h1>
+        <h1>
+          <marquee>
+            🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪Look me🍪🍪🍪🍪🍪🍪Hello🍪🍪🍪🍪🍪🍪
+            Look at me !
+          </marquee>
+        </h1>
+        <h1>
+          <marquee>
+            🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪Look me🍪🍪🍪🍪🍪🍪Hello🍪🍪🍪🍪🍪🍪
+            Look at me !
+          </marquee>
+        </h1>
+        <h1>
+          <marquee>
+            🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪Look me🍪🍪🍪🍪🍪🍪Hello🍪🍪🍪🍪🍪🍪
+            Look at me !
+          </marquee>
+        </h1>
+        <h1>
+          <marquee>
+            🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪Look me🍪🍪🍪🍪🍪🍪Hello🍪🍪🍪🍪🍪🍪
+            Look at me !
+          </marquee>
+        </h1>
+        <h1>
+          <marquee>
+            🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪Look me🍪🍪🍪🍪🍪🍪Hello🍪🍪🍪🍪🍪🍪
+            Look at me !
+          </marquee>
+        </h1>
+        <h1>
+          <marquee>
+            🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪Look me🍪🍪🍪🍪🍪🍪Hello🍪🍪🍪🍪🍪🍪
+            Look at me !
+          </marquee>
+        </h1>
+        <h1>
+          <marquee>
+            🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪Look me🍪🍪🍪🍪🍪🍪Hello🍪🍪🍪🍪🍪🍪
+            Look at me !
+          </marquee>
+        </h1>
+        <h1>
+          <marquee>
+            🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪Look me🍪🍪🍪🍪🍪🍪Hello🍪🍪🍪🍪🍪🍪
+            Look at me !
+          </marquee>
+        </h1>
+        <h1>
+          <marquee>
+            🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪Look me🍪🍪🍪🍪🍪🍪Hello🍪🍪🍪🍪🍪🍪
+            Look at me !
+          </marquee>
+        </h1>
+        <h1>
+          <marquee>
+            🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪Look me🍪🍪🍪🍪🍪🍪Hello🍪🍪🍪🍪🍪🍪
+            Look at me !
+          </marquee>
+        </h1>
+        <h1>
+          <marquee>
+            🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪Look me🍪🍪🍪🍪🍪🍪Hello🍪🍪🍪🍪🍪🍪
+            Look at me !
+          </marquee>
+        </h1>
+        <h1>
+          <marquee>
+            🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪Look me🍪🍪🍪🍪🍪🍪Hello🍪🍪🍪🍪🍪🍪
+            Look at me !
+          </marquee>
+        </h1>
+        <h1>
+          <marquee>
+            🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪Look me🍪🍪🍪🍪🍪🍪Hello🍪🍪🍪🍪🍪🍪
+            Look at me !
+          </marquee>
+        </h1>
+        <h1>
+          <marquee>
+            🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪Look me🍪🍪🍪🍪🍪🍪Hello🍪🍪🍪🍪🍪🍪
+            Look at me !
+          </marquee>
+        </h1>
+        <h1>
+          <marquee>
+            🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪Look me🍪🍪🍪🍪🍪🍪Hello🍪🍪🍪🍪🍪🍪
+            Look at me !
+          </marquee>
+        </h1>
+        <h1>
+          <marquee>
+            🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪Look me🍪🍪🍪🍪🍪🍪Hello🍪🍪🍪🍪🍪🍪
+            Look at me !
+          </marquee>
+        </h1>
+        <h1>
+          <marquee>
+            🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪Look me🍪🍪🍪🍪🍪🍪Hello🍪🍪🍪🍪🍪🍪
+            Look at me !
+          </marquee>
+        </h1>
+        <h1>
+          <marquee>
+            🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪Look me🍪🍪🍪🍪🍪🍪Hello🍪🍪🍪🍪🍪🍪
+            Look at me !
+          </marquee>
+        </h1>
+        <h1>
+          <marquee>
+            🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪Look me🍪🍪🍪🍪🍪🍪Hello🍪🍪🍪🍪🍪🍪
+            Look at me !
+          </marquee>
+        </h1>
+        <h1>
+          <marquee>
+            🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪Look me🍪🍪🍪🍪🍪🍪Hello🍪🍪🍪🍪🍪🍪
+            Look at me !
+          </marquee>
+        </h1>
+        <h1>
+          <marquee>
+            🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪Look me🍪🍪🍪🍪🍪🍪Hello🍪🍪🍪🍪🍪🍪
+            Look at me !
+          </marquee>
+        </h1>
+        <h1>
+          <marquee>
+            🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪Look me🍪🍪🍪🍪🍪🍪Hello🍪🍪🍪🍪🍪🍪
+            Look at me !
+          </marquee>
+        </h1>
+        <h1>
+          <marquee>
+            🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪Look me🍪🍪🍪🍪🍪🍪Hello🍪🍪🍪🍪🍪🍪
+            Look at me !
+          </marquee>
+        </h1>
+        <h1>
+          <marquee>
+            🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪Look me🍪🍪🍪🍪🍪🍪Hello🍪🍪🍪🍪🍪🍪
+            Look at me !
+          </marquee>
+        </h1>
+        <h1>
+          <marquee>
+            🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪Look me🍪🍪🍪🍪🍪🍪Hello🍪🍪🍪🍪🍪🍪
+            Look at me !
+          </marquee>
+        </h1>
+        <h1>
+          <marquee>
+            🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪Look me🍪🍪🍪🍪🍪🍪Hello🍪🍪🍪🍪🍪🍪
+            Look at me !
+          </marquee>
+        </h1>
+        <h1>
+          <marquee>
+            🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪Look me🍪🍪🍪🍪🍪🍪Hello🍪🍪🍪🍪🍪🍪
+            Look at me !
+          </marquee>
+        </h1>
+        <h1>
+          <marquee>
+            🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪Look me🍪🍪🍪🍪🍪🍪Hello🍪🍪🍪🍪🍪🍪
+            Look at me !
+          </marquee>
+        </h1>
+        <h1>
+          <marquee>
+            🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪Look me🍪🍪🍪🍪🍪🍪Hello🍪🍪🍪🍪🍪🍪
+            Look at me !
+          </marquee>
+        </h1>
+        <h1>
+          <marquee>
+            🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪Look me🍪🍪🍪🍪🍪🍪Hello🍪🍪🍪🍪🍪🍪
+            Look at me !
+          </marquee>
+        </h1>
+        <h1>
+          <marquee>
+            🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪Look me🍪🍪🍪🍪🍪🍪Hello🍪🍪🍪🍪🍪🍪
+            Look at me !
+          </marquee>
+        </h1>
+        <h1>
+          <marquee>
+            🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪Look me🍪🍪🍪🍪🍪🍪Hello🍪🍪🍪🍪🍪🍪
+            Look at me !
+          </marquee>
+        </h1>
+        <h1>
+          <marquee>
+            🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪Look me🍪🍪🍪🍪🍪🍪Hello🍪🍪🍪🍪🍪🍪
+            Look at me !
+          </marquee>
+        </h1>
+        <h1>
+          <marquee>
+            🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪Look me🍪🍪🍪🍪🍪🍪Hello🍪🍪🍪🍪🍪🍪
+            Look at me !
+          </marquee>
+        </h1>
+        <h1>
+          <marquee>
+            🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪Look me🍪🍪🍪🍪🍪🍪Hello🍪🍪🍪🍪🍪🍪
+            Look at me !
+          </marquee>
+        </h1>
+        <h1>
+          <marquee>
+            🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪Look me🍪🍪🍪🍪🍪🍪Hello🍪🍪🍪🍪🍪🍪
+            Look at me !
+          </marquee>
+        </h1>
+        <h1>
+          <marquee>
+            🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪Look me🍪🍪🍪🍪🍪🍪Hello🍪🍪🍪🍪🍪🍪
+            Look at me !
+          </marquee>
+        </h1>
+      </header>
+    </>
+  );
+}
+
+Index.getInitialProps = async function(ctx) {
+  setCookie(ctx, "lookatme", "YT4RR8", {
+    maxAge: 30 * 24 * 60 * 60,
+    path: "/"
+  });
+  return {};
+};
---
+++ pages/YT4RR8.js
@@ -0,0 +1,47 @@
+export default function Index() {
+  return (
+    <>
+      <header>
+        <nav></nav>
+        <h1>🎉💪Vous avez réussi !!🎉💪 </h1>
+        <p>CTRL+K sur Slack -> 'siekooc' -> rejoindre le channel</p>
+        <br />
+        <p>
+          Puis <b>postez un screenshot</b> de cette page pour prouver votre
+          réussite !💪
+        </p>
+        <br />
+        <p>Bonus de classe si les coupes sont alignées horizontalement !</p>
+        <br />
+        <marquee
+          direction="right"
+          behavior="alternate"
+          scrolldelay="5"
+          truespeed="true"
+        >
+          💨💨💨💨💨💨💨💨💨
+        </marquee>
+        <marquee direction="left" scrolldelay="5" truespeed="true">
+          🏆🏆🏆🏆🏆🏆🏆🏆🏆
+        </marquee>
+        <marquee direction="right" scrolldelay="5" truespeed="true">
+          🏆🏆🏆🏆🏆🏆🏆🏆🏆
+        </marquee>
+        <marquee direction="left" scrolldelay="5" truespeed="true">
+          🏆🏆🏆🏆🏆🏆🏆🏆🏆
+        </marquee>
+        <marquee direction="right" scrolldelay="5" truespeed="true">
+          🏆🏆🏆🏆🏆🏆🏆🏆🏆
+        </marquee>
+        <marquee
+          direction="left"
+          behavior="alternate"
+          scrolldelay="5"
+          truespeed="true"
+        >
+          💨💨💨💨💨💨💨💨💨
+        </marquee>
+      </header>
+    </>
+  );
+}
