c8851f558327ce2c17a7c73e13710e71bc3be6bf
Basile Beldame <basile.beldame@oyez.fr>
2020-04-01 19:17:12 +0200

    update

--- pages/BIMLM8.js
+++ pages/BIMLM8.js
@@ -8,33 +8,19 @@ export default function Index() {
         </h1>
         <br />
         <p>
-          Le microservice <i>cactus</i> du cluster principal du projet GCP
+          Le microservice <i>parchemin</i> du cluster <a target="_blank" href="https://console.cloud.google.com/kubernetes/list?project=test-hamdi&folder&organizationId&cloudshell=false">my-second-cluster</a> <em>(désolé pour le nom pourri)</em>
           <b>
             {" "}
-            <a target="_blank" href="#">
-              treasure-catch1
-            </a>
           </b>{" "}
-          semble vouloir <em>dire</em> quelque chose..
+          peut vous apporter des réponses..
           <br />
         </p>
         <br />
         <p>
-          Pour cette étape, vous allez peut-être avoir besoin de ces
-          instructions : LIEN_VERS_NOTION
+          <em>Pour cette étape, vous allez peut-être avoir besoin de ces
+          instructions <a target="_blank" href="https://www.notion.so/Faire-des-trucs-sur-Kube-1e5652c235594fc88805a31b0899c947">sur le Notion Oyez</a></em>
         </p>
       </header>
     </>
   );
 }
-
-/*
- ** TODO: Projet gcp avec microservice
- ** Celui ci log : GET /indice ;)
- ** L'indice c'est un tableau géant de string, dont l'une est un JWToken
- ** Avec le tableau, un id pour une nouvelle page HTML ici
- ** Dans le JWToken y'a un voiciLeProchainId: R4EY2Q
- ** Ensuite prochaine page HTML..
- ** Elle dit juste comment rejoindre le channel Slack des vainqueurs. Fin.
- ** Je pourrais mettre un slackbot qui répond à un nouvel id à la fin..
- */
